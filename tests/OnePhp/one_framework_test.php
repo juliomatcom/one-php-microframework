60bbd6de3aa15fd7abb7cb9949e3596246efbd19
Julio C <juliomatcom@gmail.com>
2015-05-31 17:44:55 +0200

    added phpunit tests

---
+++ tests/OnePhp/one_framework_test.php
@@ -0,0 +1,19 @@
+<?php
+class MoneyTest extends PHPUnit_Framework_TestCase
+{
+    // ...
+
+    public function testCanBeNegated()
+    {
+        // Arrange
+        $a = new Money(1);
+
+        // Act
+        $b = $a->negate();
+
+        // Assert
+        $this->assertEquals(-1, $b->getAmount());
+    }
+
+    // ...
+}
